06d6fccdf8ddaef88b57b2c02dfafb37179e1a34
Piotr Golyzniak <piotr.golyzniak@nordicsemi.no>
2021-08-30 12:23:01 +0200

    Add nRF52840 QEMU platform
    It allow to run upstream Zephyr tests via Twister. It demand previously
    installed and compiled QEMU emulator with added nRF52840DK platform.
    
    Signed-off-by: Piotr Golyzniak <piotr.golyzniak@nordicsemi.no>

---
+++ boards/arm/qemu_nrf52840dk_nrf52840/board.cmake
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: Apache-2.0
+
+set(ENV{QEMU_BIN_PATH} /home/redbeard/Nordic/qemu/bin/debug/native)
+
+set(EMU_PLATFORM qemu)
+
+set(QEMU_CPU_TYPE_${ARCH} cortex-m4)
+set(QEMU_FLAGS_${ARCH}
+  -cpu ${QEMU_CPU_TYPE_${ARCH}}
+  -machine nRF52840DK
+  -nographic
+  -vga none
+  )
+
+board_set_debugger_ifnset(qemu)
