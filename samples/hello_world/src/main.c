d1b5e043b09065ca0cd4f5d592ec01b902e9746e
Piotr Golyzniak <piotr.golyzniak@nordicsemi.no>
2021-09-15 11:45:19 +0200

    Modify Hello World sample and add 10 sec wait inside program

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,8 +6,29 @@
 
 #include <zephyr.h>
 #include <sys/printk.h>
+#include <timing/timing.h>
 
 void main(void)
 {
+	timing_t start_time, end_time;
+	uint64_t total_cycles;
+	uint64_t total_ns;
+
+	timing_init();
+	timing_start();
+
+	start_time = timing_counter_get();
+
+	k_busy_wait(10000000); // 10 sec
+
+	end_time = timing_counter_get();
+
+	total_cycles = timing_cycles_get(&start_time, &end_time);
+	total_ns = timing_cycles_to_ns(total_cycles);
+
+	printk("Elapsed time: %lld [cycles] | %lld [ns]\n", total_cycles, total_ns);
+
+	timing_stop();
+
 	printk("Hello World! %s\n", CONFIG_BOARD);
 }
