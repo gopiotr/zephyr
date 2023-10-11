8d1722010fe4def8e609d89eebfa22481dd796a9
Piotr Golyzniak <piotr.golyzniak@nordicsemi.no>
2023-10-11 12:43:39 +0200

    tests: add multi_build test
    Signed-off-by: Piotr Golyzniak <piotr.golyzniak@nordicsemi.no>

---
+++ tests/multi_build/sysbuild.cmake
@@ -0,0 +1,3 @@
+
+ExternalZephyrProject_Add(APPLICATION peripheral_hr SOURCE_DIR ${ZEPHYR_BASE}/samples/bluetooth/peripheral_hr)
+ExternalZephyrProject_Add(APPLICATION central_hr SOURCE_DIR ${ZEPHYR_BASE}/samples/bluetooth/central_hr)
